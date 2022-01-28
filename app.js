800d2429476a3506cdabc213bec0c737b9123d2b
Carlos N Reina <cnreina@gmail.com>
2022-01-28 15:25:30 -0700

    setup

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const feedRoutes = require('./routes/feed');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const feedRoutes    = require('./routes/feed');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
--- routes/feed.js
+++ routes/feed.js
@@ -1,8 +1,6 @@
-const express = require('express');
-
-const feedController = require('../controllers/feed');
-
-const router = express.Router();
+const express           = require('express');
+const feedController    = require('../controllers/feed');
+const router            = express.Router();
 
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
